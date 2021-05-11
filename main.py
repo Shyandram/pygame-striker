510bd6c00dcfd646992e6af1d0b9d2cbf2cbac60
Shyandram <shyangen104@gmail.com>
2021-05-11 14:36:31 +0800

    player movement

--- main.py
+++ main.py
@@ -13,10 +13,38 @@ movingScale = 600/fps
 
 player = Player(playground=playground,sensitivity = movingScale)
 
-
+KeyCountX = 0
+KeyCountY = 0
 
 while running:
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                KeyCountX += 1
+                player.to_the_left()
+            if event.key == pygame.K_d:
+                KeyCountX -= 1
+                player.to_the_right()
+            if event.key == pygame.K_w:
+                KeyCountY -= 1
+                player.to_the_bottom()
+            if event.key == pygame.K_s:
+                KeyCountY += 1
+                player.to_the_top()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a or pygame.K_d == event.key:
+                if KeyCountX == 1:
+                    KeyCountX = 0
+                    player.stop_x()
+                else:
+                    KeyCountX -= 1
+            if event.key == pygame.K_s or pygame.K_w == event.key:
+                if KeyCountY == 1:
+                    KeyCountY = 0
+                    player.stop_y()
+                else:
+                    KeyCountY -= 1
+                
         if event.type == pygame.quit:
             running = False
     screen.blit(background,(0,0))
