d19419bac508dbcfcdb58404ac771576cb614a91
Shyandram <shyangen104@gmail.com>
2021-05-11 13:40:34 +0800

    fix problem, xy none solved

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import pygame
-from res.setting import screen,background
+from res.setting import *
+from attitude.game_object import  *
+from attitude.Player import  *
+
 pygame.init()
+
+clock = pygame.time.Clock()
 
 running = True
 fps = 60 
-clock = pygame.time.Clock()
+movingScale = 600/fps
+
+player = Player(playground=playground,sensitivity = movingScale)
+
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.quit:
             running = False
     screen.blit(background,(0,0))
+    
+    player.update()
+    screen.blit(player.image,player.xy)
     pygame.display.update()
     dt = clock.tick(fps)
 pygame.quit()
